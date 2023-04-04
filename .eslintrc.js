1bce80fdf2e7ae994a3130153736c1c97a1b029c
moxiaoluo <xiaoluolx@163.com>
2023-04-04 14:17:57 +0800

    docs: ecmaversion update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:react/recommended', 'google'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
