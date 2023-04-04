8b965919a25ab3387d6dd6924f853b1a2c447d33
moxiaoluo <xiaoluolx@163.com>
2023-04-04 14:39:12 +0800

    style: test format

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Adds two numbers together.
+ * @param {int} a The first number.
+ * @param {int} b The second number.
+ * @return {int} The sum of the two numbers.
+ */
+function add(a, b) {
+  return a + b;
+}
+const result = add(1, 2);
+console.log(result);
